b15b70020273129ae16b97fe24d87d7e348e8c4b
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-16 14:03:39 +0400

    add: New data file with initial links array

---
+++ lib/data.ts
@@ -0,0 +1,20 @@
+export const links = [
+    {
+        name: "Home",
+        path: "/",
+    },
+    {
+        name: "About",
+        path: "/about",
+    },
+    {
+        name: "Contact",
+        path: "/contact",
+    },
+    {
+        name: "Blog",
+        path: "/blog",
+    },
+] as const;
+
+export const yearsOfExperience = 4;
