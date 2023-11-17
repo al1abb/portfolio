43b75598874b020c0e8e7a8b78244a125ba125c0
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-17 19:50:14 +0400

    refactor: Updated icons and text sizes for intro
    In addition, twitter/x icon is added

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,50 +1,63 @@
 import Image from "next/image";
-import { yearsOfExperience } from "@/lib/data";
+
+// NextUI
 import { Button, Link } from "@nextui-org/react";
+
+// Data
+import { yearsOfExperience } from "@/lib/data";
 
 // Icons
 import { LuDownload } from "react-icons/lu";
-import { FaGithub } from "react-icons/fa";
-import { BsLinkedin } from "react-icons/bs";
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
 
 type Props = {};
 
 export default function Intro(props: Props) {
     return (
         <>
-            <div className="shadow-lg rounded-full border-8 border-solid border-green-500">
+            <div className="relative shadow-lg rounded-full w-fit h-fit border-8 border-solid border-green-400">
                 <Image
-                    className="rounded-full"
-                    src={"/profile.jpg"}
+                    className="rounded-full relative z-10"
+                    src={"/assets/profile.jpg"}
                     width={150}
                     height={150}
-                    alt={""}
+                    alt={"Ali Abbasov"}
                 ></Image>
+
+                <div className="z-0 absolute w-[15rem] h-[15rem] top-0 right-0 bg-green-500 rounded-full blur-[12rem] xs:blur-[30rem] sm:blur-[20rem] md:blur-[10rem] lg:blur-[10rem] xl:blur-[13rem]"></div>
             </div>
 
-            <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-4 z-10">
                 <p className="text-4xl max-w-[50rem] leading-snug">
-                    Hi, I&apos;m <b>Ali Abbasov</b>, a full-stack web developer
-                    with over {yearsOfExperience} years of experience creating
-                    dynamic, responsive solutions for seamless user experiences.
+                    Hi, I&apos;m <span className="font-bold">Ali Abbasov</span>,
+                    a full-stack web developer with over {yearsOfExperience}{" "}
+                    years of experience creating dynamic, responsive solutions
+                    for seamless user experiences.
                 </p>
             </div>
 
             <div className="flex flex-wrap gap-10">
-                <Button radius="full" disableRipple>
+                <Button radius="full" disableRipple className="text-lg">
                     Contact me
                 </Button>
-                <Button radius="full" color="secondary" disableRipple>
+                <Button
+                    radius="full"
+                    color="secondary"
+                    disableRipple
+                    className="text-lg"
+                >
                     Download CV
                     <LuDownload size={24} />
                 </Button>
                 <Link href="https://github.com/aliabb01" target="_blank">
-                    {/* <Github color="black" /> */}
-                    <FaGithub color="black" size={35} />
+                    <FaGithubSquare size={40} />
                 </Link>
                 <Link>
-                    {/* <Linkedin color="black" /> */}
-                    <BsLinkedin color="black" size={35} />
+                    <FaSquareXTwitter size={40} />
+                </Link>
+                <Link>
+                    <FaLinkedin size={40} />
                 </Link>
             </div>
         </>
