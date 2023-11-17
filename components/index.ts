846365d24a4a9da1c86f865cb20e3107fa7ac7d6
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-17 19:50:58 +0400

    feat: Add components exporter

---
+++ components/index.ts
@@ -0,0 +1,7 @@
+import Navbar from "./Navbar";
+
+import Intro from "./Intro";
+import Projects from "./Projects";
+import ProjectCard from "./ProjectCard";
+
+export { Navbar, Intro, Projects, ProjectCard };
