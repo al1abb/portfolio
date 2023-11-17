300d77594627d6f522e4238082ddac094375877a
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-17 19:51:39 +0400

    refactor: Add spacer between intro and projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import Intro from "@/components/Intro";
+// NextUI
+import { Spacer } from "@nextui-org/react";
+
+// Components
+import { Intro, Projects } from "@/components";
 
 export default function Home() {
     return (
         <main className="flex flex-col gap-4 items-center justify-between py-5 px-5">
             <Intro />
+            <Spacer y={20} />
+            <Projects />
         </main>
     );
 }
