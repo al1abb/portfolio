1da24c611b795a4385cf76072134ca8e56016451
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-18 19:21:57 +0400

    feat: Added initial about component

---
+++ components/about.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { SectionHeading } from "@/components";
+
+type Props = {};
+
+export default function About({}: Props) {
+    return (
+        <section id="about" className="max-w-[45rem] text-center leading-8">
+            <SectionHeading>About me</SectionHeading>
+            <p className="mb-3">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Ducimus quas iste ipsa iure magni molestiae sint. Dicta optio
+                iure odio.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+                minus, repellendus expedita dolores quod eaque. Quisquam facilis
+                natus ullam id accusamus deleniti porro aliquid aut maxime
+                asperiores, ipsa quis fugit.
+            </p>
+        </section>
+    );
+}
