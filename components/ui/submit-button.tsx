cecdffab897a2b22d786a74bb052f0ab6b614183
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-19 15:43:09 +0400

    refactor: Export submit button into a new component
    Contact form submit button exported to utilize useFormStatus

---
+++ components/ui/submit-button.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+
+// NextUI
+import { Button } from "@nextui-org/react";
+
+// Icons
+import { FaPaperPlane } from "react-icons/fa";
+
+export default function SubmitButton() {
+    const { pending } = useFormStatus();
+
+    return (
+        <Button type="submit" radius="sm" className="group" isLoading={pending}>
+            {pending ? "Sending..." : "Send"}{" "}
+            <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
+        </Button>
+    );
+}
