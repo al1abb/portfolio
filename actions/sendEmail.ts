deb34ff6c78e87bf0402ea83522d5d77066ff990
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-19 15:39:34 +0400

    feat: Add send email action file
    This commit implements initial logic for sending contact form email

---
+++ actions/sendEmail.ts
@@ -0,0 +1,55 @@
+"use server";
+// ? Having use server at the very top will make all functions here run on the server
+
+// Nodemailer
+import nodemailer, { SendMailOptions } from "nodemailer";
+
+// React Email
+import { render } from "@react-email/render"; // This is for rendering the email
+
+// Email template
+import ContactFormEmail from "@/templates/email/contact-form-email";
+
+// Utils
+import { getErrorMessage } from "@/lib/utils";
+
+// Constants
+import { NODEMAILER_EMAIL, NODEMAILER_PW } from "@/lib/constants";
+
+// Nodemailer transporter
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: NODEMAILER_EMAIL,
+        pass: NODEMAILER_PW,
+    },
+});
+
+export const sendEmail = async (formData: FormData) => {
+    // Get the values from the form data
+    const senderEmail = formData.get("email") as string;
+    const message = formData.get("message") as string;
+
+    // Render the email to html
+    const emailHtml = render(ContactFormEmail({ senderEmail, message }));
+
+    let data;
+    try {
+        const mailOptions: SendMailOptions = {
+            from: "Portfolio Contact Form",
+            to: NODEMAILER_EMAIL,
+            subject: "New message from portfolio contact form",
+            html: emailHtml,
+        };
+
+        // Send email
+        data = await transporter.sendMail(mailOptions);
+    } catch (error: unknown) {
+        return {
+            error: getErrorMessage(error),
+        };
+    }
+    return {
+        data,
+    };
+};
