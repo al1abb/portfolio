6f481537f7ee62efa6e8ecedacfd3dc644943ef4
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-19 15:43:27 +0400

    feat: Add initial footer component

---
+++ components/footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <footer className="py-5 px-4 text-center text-gray-500">
+            <small>
+                &copy; {new Date().getFullYear()} Ali Abbasov. All rights
+                reserved.
+            </small>
+        </footer>
+    );
+}
