00b642c6976f344aae9bfe7b7df5adb25e948d2d
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-19 15:45:19 +0400

    chore: Updated components exporter
    New footer and submit-button components

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,5 @@
 import Navbar from "./navbar";
+import Footer from "./footer";
 
 import SectionHeading from "./section-heading";
 
@@ -7,4 +8,15 @@ import About from "./about";
 import Projects from "./projects";
 import Contact from "./contact";
 
-export { Navbar, SectionHeading, Intro, About, Projects, Contact };
+import SubmitButton from "./ui/submit-button";
+
+export {
+    Navbar,
+    Footer,
+    SectionHeading,
+    Intro,
+    About,
+    Projects,
+    Contact,
+    SubmitButton,
+};
