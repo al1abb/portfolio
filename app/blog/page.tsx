a80ed90c940f0a6e488b5fbc7d400f535a2f2210
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-20 17:44:55 +0400

    feat: Initial blog page showing all posts

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,32 @@
-import React from "react";
+import Link from "next/link";
 
-type Props = {};
+import { getSortedPostsData } from "@/lib/posts";
 
-const BlogPage = (props: Props) => {
-    return <div>BlogPage</div>;
+import { Post } from "@/types";
+import { SectionHeading } from "@/components";
+
+type PostWithoutContent = Omit<Post, "content">;
+
+const BlogPage = async () => {
+    const allPostsData: PostWithoutContent[] = await getSortedPostsData();
+
+    return (
+        <section className="flex flex-col items-center py-5 px-5">
+            <SectionHeading>My Blog</SectionHeading>
+            <ul>
+                {allPostsData.map(({ id, title, date }) => (
+                    <li key={id}>
+                        <div className="font-medium mb-1 mt-5">
+                            <Link href={`/blog/${id}`}>{title}</Link>
+                        </div>
+                        <small className="text-gray-500 font-medium">
+                            {date}
+                        </small>
+                    </li>
+                ))}
+            </ul>
+        </section>
+    );
 };
 
 export default BlogPage;
