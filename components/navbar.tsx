27abcab346e0e4b54926c6bb3efee7813f077919
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-21 16:41:18 +0400

    refactor: Center and narrow the navbar, add menu toggle

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,29 +1,56 @@
+"use client";
+
+import { useState } from "react";
+
 import Link from "next/link";
 
 // Data
 import { links } from "@/lib/data";
+
+// NextUI
 import {
     Navbar as NextUINavbar,
     NavbarBrand,
     NavbarContent,
     NavbarItem,
     Button,
+    NavbarMenuToggle,
+    NavbarMenu,
+    NavbarMenuItem,
 } from "@nextui-org/react";
 
 export default function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     return (
         <NextUINavbar
             isBlurred
             shouldHideOnScroll
-            className="z-[999] rounded-lg"
+            className="z-[999] sm:rounded-xl sm:w-[70%] mx-auto"
         >
-            <NavbarContent className="sm:flex">
+            <NavbarContent>
+                <NavbarMenuToggle
+                    aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+                    className="sm:hidden"
+                />
+            </NavbarContent>
+            <NavbarContent className="hidden sm:flex" justify="start">
                 {links.map((link) => (
                     <NavbarItem key={link.name}>
                         <Link href={link.path}>{link.name}</Link>
                     </NavbarItem>
                 ))}
             </NavbarContent>
+            <NavbarContent></NavbarContent>
+
+            <NavbarMenu>
+                {links.map((link, index) => (
+                    <NavbarMenuItem key={`${link.name}-${index}`}>
+                        <Link className="" href={link.path}>
+                            {link.name}
+                        </Link>
+                    </NavbarMenuItem>
+                ))}
+            </NavbarMenu>
         </NextUINavbar>
     );
 }
