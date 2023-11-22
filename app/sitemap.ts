eb49395380d90d1a971cdeb8700d9ec374d9dbd5
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-22 12:43:49 +0400

    feat: Implement sitemap feature

---
+++ app/sitemap.ts
@@ -0,0 +1,38 @@
+import { MetadataRoute } from "next";
+
+// Posts
+import { getSortedPostsData } from "@/lib/posts";
+
+// Constants
+import { WEBSITE_URL } from "@/lib/constants";
+
+// Types
+import { PostWithoutContent } from "@/types";
+
+type ChangeFrequencyType =
+    | "always"
+    | "hourly"
+    | "daily"
+    | "weekly"
+    | "monthly"
+    | "yearly"
+    | "never";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+    const changeFrequency: ChangeFrequencyType = "daily";
+    const allPostsData: PostWithoutContent[] = await getSortedPostsData();
+
+    const posts = allPostsData.map(({ id, date }) => ({
+        url: `${WEBSITE_URL}/blog/${id}`,
+        lastModified: date,
+        changeFrequency,
+    }));
+
+    const routes = ["", "/blog"].map((route) => ({
+        url: `${WEBSITE_URL}${route}`,
+        lastModified: new Date().toISOString(),
+        changeFrequency,
+    }));
+
+    return [...routes, ...posts];
+}
