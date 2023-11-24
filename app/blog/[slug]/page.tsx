b1bb6d211b649249c4e010ba45d76c82cc73db65
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-24 20:49:01 +0400

    refactor: Update tags styles, new published date format
    In addition, spacing issues has been fixed

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -9,6 +9,7 @@ import { ProgressBar } from "@/components";
 
 // Lib
 import { getPostsMeta, getPostByName } from "@/lib/posts";
+import getFormattedDate from "@/lib/utils";
 
 export const revalidate = 10;
 
@@ -51,20 +52,29 @@ export default async function BlogPostPage({ params }: Props) {
 
     const { meta, content } = post;
 
+    const pubDate = getFormattedDate(meta.date);
+
     const tags = meta.tags.map((tag, i) => (
-        <Link key={i} href={`/blog/tags/${tag}`} className="capitalize">
+        <Link
+            key={i}
+            href={`/blog/tags/${tag}`}
+            className="bg-gray-300 text-gray-800 dark:bg-blue-700 dark:text-white rounded-full px-2 text-base no-underline"
+        >
             {tag}
         </Link>
     ));
 
     return (
-        <div className="container mx-auto prose prose-xl prose-slate dark:prose-invert">
+        <div className="container mx-auto px-5 prose prose-xl prose-slate dark:prose-invert">
             <ProgressBar />
+
             <h2 className="font-extrabold text-4xl mb-1">{meta.title}</h2>
 
-            <p className="text-gray-500 font-medium mb-5">{meta.date}</p>
+            <p className="text-gray-500 dark:text-gray-400 font-regular text-base mb-5">
+                Published on {pubDate}   |   {meta.readingTime}
+            </p>
 
-            <p>{meta.readingTime}</p>
+            <div className="flex flex-row gap-4">{tags}</div>
 
             <article>{content}</article>
 
@@ -73,9 +83,9 @@ export default async function BlogPostPage({ params }: Props) {
                 <div className="flex flex-row gap-4">{tags}</div>
             </section>
 
-            <Link href="/blog" className="mt-5">
-                ← Back to blog page
-            </Link>
+            <div className="mt-8">
+                <Link href="/blog">← Back to blog page</Link>
+            </div>
         </div>
     );
 }
