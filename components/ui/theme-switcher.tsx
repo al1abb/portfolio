d1ecc2d083114c711c4b90316bc009e4a7d00d14
Ali Abbasov <ali3abbasov@gmail.com>
2023-11-29 12:12:49 +0400

    chore: Add comments to theme switcher

--- components/ui/theme-switcher.tsx
+++ components/ui/theme-switcher.tsx
@@ -2,25 +2,27 @@
 
 import { useTheme } from "next-themes";
 
+// NextUI
 import { Switch } from "@nextui-org/react";
+
+// Icons
 import { FaMoon, FaSun } from "react-icons/fa";
 
 export default function ThemeSwitcher() {
     const { theme, setTheme } = useTheme();
+
     return (
-        <>
-            <Switch
-                defaultSelected={theme === "dark"}
-                onChange={() => setTheme(theme === "dark" ? "light" : "dark")}
-                size="lg"
-                thumbIcon={({ isSelected, className }) =>
-                    isSelected ? (
-                        <FaMoon className={`className text-blue-700`} />
-                    ) : (
-                        <FaSun className={`className text-yellow-500`} />
-                    )
-                }
-            ></Switch>
-        </>
+        <Switch
+            size="lg"
+            isSelected={theme == "dark" ? true : false}
+            onChange={() => setTheme(theme === "dark" ? "light" : "dark")}
+            thumbIcon={({ isSelected, className }) =>
+                isSelected ? (
+                    <FaMoon className={`className text-blue-700`} />
+                ) : (
+                    <FaSun className={`className text-yellow-500`} />
+                )
+            }
+        />
     );
 }
