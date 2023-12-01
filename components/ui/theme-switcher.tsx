0f205de5b4fb0ff9b477133bc12a7416886f34f2
Ali Abbasov <ali3abbasov@gmail.com>
2023-12-01 14:00:29 +0400

    fix: Add mounted state with skeleton fallback to theme switcher

--- components/ui/theme-switcher.tsx
+++ components/ui/theme-switcher.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
+// Next themes
 import { useTheme } from "next-themes";
 
 // NextUI
-import { Switch } from "@nextui-org/react";
+import { Skeleton, Switch } from "@nextui-org/react";
 
 // Icons
 import { FaMoon, FaSun } from "react-icons/fa";
@@ -11,10 +14,19 @@ import { FaMoon, FaSun } from "react-icons/fa";
 export default function ThemeSwitcher() {
     const { theme, setTheme } = useTheme();
 
+    // ? Without this mounted state check the theme switcher won't work as it becomes undefined on first render
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) return <Skeleton className="rounded-full w-14 h-8 mr-2" />;
+
     return (
         <Switch
             size="lg"
-            isSelected={theme == "dark" ? true : false}
+            isSelected={theme === "dark" ? true : false}
             onChange={() => setTheme(theme === "dark" ? "light" : "dark")}
             thumbIcon={({ isSelected, className }) =>
                 isSelected ? (
