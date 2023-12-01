6ce58974cd1165ab10318c812919b6c39bea7c00
Ali Abbasov <ali3abbasov@gmail.com>
2023-12-01 13:58:39 +0400

    refactor: Enable system theme check in providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,7 +6,11 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <NextUIProvider>
-            <NextThemesProvider attribute="class" defaultTheme="dark">
+            <NextThemesProvider
+                attribute="class"
+                defaultTheme="light"
+                enableSystem={true}
+            >
                 {children}
             </NextThemesProvider>
         </NextUIProvider>
