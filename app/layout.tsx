734127485cddf1172b388e7acc51bdd905ff4c08
Ali Abbasov <ali3abbasov@gmail.com>
2023-12-02 19:46:04 +0400

    refactor: Add keywords and initial json ld to metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,11 +17,26 @@ import { Navbar, Footer, GoogleAnalytics } from "@/components";
 import { Toaster } from "react-hot-toast";
 
 // Constants
-import { WEBSITE_URL } from "@/lib/constants";
+import {
+    GITHUB_LINK,
+    LINKEDIN_LINK,
+    TWITTER_LINK,
+    WEBSITE_URL,
+} from "@/lib/constants";
 
 const outfit = Outfit({
     subsets: ["latin"],
 });
+
+const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Person",
+    name: "Ali Abbasov",
+    jobTitle: "Full Stack Web Developer",
+    url: "https://aliabb.vercel.app",
+    sameAs: [GITHUB_LINK, TWITTER_LINK, LINKEDIN_LINK],
+    image: "https://aliabb.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fprofile.8a952696.jpg&w=256&q=95",
+};
 
 export const metadata: Metadata = {
     title: {
@@ -30,6 +45,7 @@ export const metadata: Metadata = {
     },
     description:
         "Ali Abbasov is a full stack web developer with over 4 years of experience.",
+    keywords: ["Ali Abbasov", "Full Stack Web Developer", "aliabb01"],
     icons: {
         icon: Favicon.src,
     },
@@ -52,6 +68,13 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
+            <head>
+                <script
+                    type="application/ld+json"
+                    id="json-ld"
+                    dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+                />
+            </head>
             <body
                 className={`${outfit.className} bg-slate-100 text-gray-950 relative min-h-screen dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
             >
