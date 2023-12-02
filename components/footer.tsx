b7e51b1c1cf58d995d2f2d22845a2b60369cc655
Ali Abbasov <ali3abbasov@gmail.com>
2023-12-02 14:36:38 +0400

    refactor: Update footer text colors for light and dark mode

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="py-5 px-4 text-center text-gray-500">
+        <footer className="py-5 px-4 text-center text-gray-600 dark:text-gray-300">
             <small>
                 &copy; {new Date().getFullYear()} Ali Abbasov. All rights
                 reserved.
