0ec183c0d9d7f3d28b91665354bdde34d140dff9
Ali Abbasov <ali3abbasov@gmail.com>
2023-12-03 15:32:54 +0400

    feat: New seo.ts file with initial jsonLd object

---
+++ lib/seo.ts
@@ -0,0 +1,36 @@
+// Constants
+import {
+    GITHUB_LINK,
+    LINKEDIN_LINK,
+    TWITTER_LINK,
+    WEBSITE_URL,
+} from "@/lib/constants";
+
+export const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Person",
+    name: "Ali Abbasov",
+    jobTitle: "Full Stack Web Developer",
+    url: WEBSITE_URL,
+    sameAs: [GITHUB_LINK, TWITTER_LINK, LINKEDIN_LINK],
+    image: "https://aliabb.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fprofile.8a952696.jpg&w=256&q=95",
+    mainEntityOfPage: {
+        "@type": "WebPage",
+        "@id": `${WEBSITE_URL}/#website`,
+    },
+    "@graph": [
+        {
+            "@type": "WebSite",
+            "@id": `${WEBSITE_URL}/#website`,
+            url: `${WEBSITE_URL}`,
+        },
+        {
+            "@type": "WebPage",
+            "@id": `${WEBSITE_URL}/blog/#website`,
+            url: `${WEBSITE_URL}/blog`,
+            isPartOf: {
+                "@id": `${WEBSITE_URL}/#website`,
+            },
+        },
+    ],
+};
