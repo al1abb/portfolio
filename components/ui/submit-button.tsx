370b2e2d91847180dfdb6d07774374de2a1ba50e
Ali Abbasov <ali3abbasov@gmail.com>
2023-12-04 15:30:18 +0400

    refactor: Update contact form submit btn text size

--- components/ui/submit-button.tsx
+++ components/ui/submit-button.tsx
@@ -12,9 +12,14 @@ export default function SubmitButton() {
     const { pending } = useFormStatus();
 
     return (
-        <Button type="submit" radius="sm" className="group" isLoading={pending}>
+        <Button
+            type="submit"
+            radius="sm"
+            className="group text-md"
+            isLoading={pending}
+        >
             {pending ? "Sending..." : "Send"}{" "}
-            <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
+            <FaPaperPlane className="opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
         </Button>
     );
 }
