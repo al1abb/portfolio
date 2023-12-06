047652f3c3f8b00f67ec0835c2bfc82cf188fc81
Ali Abbasov <ali3abbasov@gmail.com>
2023-12-06 13:50:29 +0400

    refactor: Make google analytics scripts lazyOnLoad

--- components/google-analytics.tsx
+++ components/google-analytics.tsx
@@ -11,12 +11,12 @@ export default function GoogleAnalytics() {
             {process.env.NODE_ENV === "production" && (
                 <>
                     <Script
-                        strategy="afterInteractive"
+                        strategy="lazyOnLoad"
                         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
                     />
                     <Script
                         id="gtag-init"
-                        strategy="afterInteractive"
+                        strategy="lazyOnLoad"
                         dangerouslySetInnerHTML={{
                             __html: gtag.DATALAYER,
                         }}
