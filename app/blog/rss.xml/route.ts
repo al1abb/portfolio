2ecc10af63462a824eb769b2f6aac881d6878bca
Ali Abbasov <ali3abbasov@gmail.com>
2023-12-08 16:33:35 +0400

    refactor: Export rss feedOptions to constants

--- app/blog/rss.xml/route.ts
+++ app/blog/rss.xml/route.ts
@@ -4,28 +4,13 @@ import { NextResponse } from "next/server";
 import { Feed } from "feed";
 
 // Lib
-import { WEBSITE_URL } from "@/lib/constants";
+import { RSS_FEED_OPTIONS, WEBSITE_URL } from "@/lib/constants";
 import { getPostsMeta } from "@/lib/posts";
 
 export async function GET() {
     const allPosts = await getPostsMeta();
 
-    const feedOptions = {
-        title: "Blog Posts | RSS Feed",
-        description: "Blog posts of Ali Abbasov",
-        id: WEBSITE_URL!,
-        link: WEBSITE_URL,
-        language: "en-us",
-        favicon: `${WEBSITE_URL}/favicon.ico`,
-        generator: "Feed for Node.js",
-        copyright: `All rights reserved ${new Date().getFullYear()}, Ali Abbasov`,
-        author: {
-            name: "Ali Abbasov",
-            link: `${WEBSITE_URL}/#contact`,
-        },
-    };
-
-    const feed = new Feed(feedOptions);
+    const feed = new Feed(RSS_FEED_OPTIONS);
 
     allPosts?.forEach((post) => {
         const categories = post.tags.map((tag) => ({ name: tag, slug: tag }));
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,6 @@
+// Data
+export const WEB_DEV_START_DATE = new Date("2020-01-23");
+
 // Nodemailer
 export const NODEMAILER_EMAIL = process.env.NODEMAILER_EMAIL;
 export const NODEMAILER_PW = process.env.NODEMAILER_PW;
@@ -5,14 +8,27 @@ export const NODEMAILER_PW = process.env.NODEMAILER_PW;
 // Github
 export const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
-// Data
-export const WEB_DEV_START_DATE = new Date("2020-01-23");
-
 // Social links
 export const GITHUB_LINK = "https://github.com/aliabb01";
 export const TWITTER_LINK = "https://twitter.com/ali3abb";
 export const LINKEDIN_LINK = "https://www.linkedin.com/in/ali-abbasov/";
 
-// Website URL
+// Website URLs
 export const WEBSITE_URL = process.env.SITE_URL;
 export const BLOG_RSS_URL = `${WEBSITE_URL}/blog/rss.xml`;
+
+// RSS
+export const RSS_FEED_OPTIONS = {
+    title: "Blog Posts | RSS Feed",
+    description: "Blog posts of Ali Abbasov",
+    id: WEBSITE_URL!,
+    link: WEBSITE_URL,
+    language: "en-us",
+    favicon: `${WEBSITE_URL}/favicon.ico`,
+    generator: "Feed for Node.js",
+    copyright: `All rights reserved ${new Date().getFullYear()}, Ali Abbasov`,
+    author: {
+        name: "Ali Abbasov",
+        link: `${WEBSITE_URL}/#contact`,
+    },
+};
