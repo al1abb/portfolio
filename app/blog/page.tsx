18ce777f5ea5768ad3ddde0dbe6389e7f3a007d4
Ali Abbasov <ali3abbasov@gmail.com>
2023-12-08 17:10:49 +0400

    refactor: Add rss feed link to blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,14 @@
 import { Metadata } from "next";
+import Link from "next/link";
 
 // Lib
 import { getPostsMeta } from "@/lib/posts";
 
 // Components
 import { PostListItem, SectionHeading } from "@/components";
+
+// Icons
+import { LuRss } from "react-icons/lu";
 
 export const revalidate = 10;
 
@@ -27,6 +31,16 @@ const BlogPage = async () => {
     return (
         <section className="flex flex-col items-center py-5 px-5">
             <SectionHeading>My Blog</SectionHeading>
+
+            <Link
+                href="/blog/rss"
+                className="mb-3 hover:underline underline-offset-4 decoration-current"
+            >
+                <p className="flex gap-2 text-[#f26522] dark:text-[#ff8c00]">
+                    <LuRss size={24} /> RSS Feed
+                </p>
+            </Link>
+
             <ul>
                 {posts.map((post) => (
                     <PostListItem key={post.slug} post={post} />
