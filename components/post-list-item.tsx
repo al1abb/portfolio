ed14009adb8365a0711adba568cbfa1146b9f639
Ali Abbasov <ali3abbasov@gmail.com>
2023-12-08 18:01:16 +0400

    refactor: Update post-list-item styles for lighthouse

--- components/post-list-item.tsx
+++ components/post-list-item.tsx
@@ -13,17 +13,14 @@ export default function PostListItem({ post }: Props) {
     return (
         <li>
             <div className="font-medium mb-1 mt-5">
-                <Link
-                    className="text-xl text-blue-400 hover:text-blue-600"
-                    href={`/blog/${slug}`}
-                >
+                <Link className="text-xl text-link" href={`/blog/${slug}`}>
                     {title}
                 </Link>
             </div>
             <p className="text-md text-gray-900 dark:text-gray-400">
                 {description}
             </p>
-            <small className="text-gray-500 dark:text-gray-100 font-medium">
+            <small className="text-gray-700 dark:text-gray-100 font-medium tracking-wider">
                 {date}
             </small>
         </li>
